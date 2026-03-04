6cae4411246625fea4607831b4dcc43c2513dd8a
crash0verride11 <3526616+crash0verride11@users.noreply.github.com>
2026-03-04 17:42:17 -0500

    fix: ruff string collapse
    Ruff is rough

--- custom_components/duke_energy/const.py
+++ custom_components/duke_energy/const.py
@@ -14,7 +14,4 @@ OAUTH2_SCOPES = ["openid", "profile", "email", "offline_access"]
 AUTH0_CLIENT = "eyJuYW1lIjoiQXV0aDAuc3dpZnQiLCJlbnYiOnsiaU9TIjoiMjYuMiIsInN3aWZ0IjoiNi54In0sInZlcnNpb24iOiIyLjEzLjAifQ"  # noqa: E501
 
 # Mobile app redirect URI - required by Duke Energy Auth0 config
-MOBILE_REDIRECT_URI = (
-    "https://login.duke-energy.com/ios/"
-    "com.duke-energy.app/callback"
-)
+MOBILE_REDIRECT_URI = "https://login.duke-energy.com/ios/com.duke-energy.app/callback"
